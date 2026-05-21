ad590e00148ee5658980612cc96d771c161f5c98
Talya Kazayof <talya8399@gmail.com>
2026-05-21 15:42:06 +0300

    Add Task REST API endpoint

--- taskmanager/src/main/java/com/talya/taskmanager/HelloController.java
+++ taskmanager/src/main/java/com/talya/taskmanager/HelloController.java
@@ -3,11 +3,22 @@ package com.talya.taskmanager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot!";
+        return "Task Manager API is running!";
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getTasks() {
+
+        Task task1 = new Task(1L, "Learn Spring Boot");
+        Task task2 = new Task(2L, "Build REST API");
+
+        return List.of(task1, task2);
     }
 }
