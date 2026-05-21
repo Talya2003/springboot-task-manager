7bb0fcd697e6080fdcd21c36fc8b197846840a0a
Talya Kazayof <talya8399@gmail.com>
2026-05-21 16:05:58 +0300

    Add POST endpoint for task creation

--- taskmanager/src/main/java/com/talya/taskmanager/HelloController.java
+++ taskmanager/src/main/java/com/talya/taskmanager/HelloController.java
@@ -1,12 +1,19 @@
 package com.talya.taskmanager;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class HelloController {
+
+    private List<Task> tasks = new ArrayList<>();
+
+    public HelloController() {
+        tasks.add(new Task(1L, "Learn Spring Boot"));
+        tasks.add(new Task(2L, "Build REST API"));
+    }
 
     @GetMapping("/")
     public String hello() {
@@ -15,10 +22,14 @@ public class HelloController {
 
     @GetMapping("/tasks")
     public List<Task> getTasks() {
+        return tasks;
+    }
 
-        Task task1 = new Task(1L, "Learn Spring Boot");
-        Task task2 = new Task(2L, "Build REST API");
+    @PostMapping("/tasks")
+    public Task createTask(@RequestBody Task task) {
 
-        return List.of(task1, task2);
+        tasks.add(task);
+
+        return task;
     }
 }
